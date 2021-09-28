d197bfce246586d101d0e17eea894fb7d73d022b
Nguyễn văn hải <nvhai2408@gmial.com>
2021-09-28 16:41:03 +0700

    update css

--- JQuery/P7_P12/P7/p7.js
+++ JQuery/P7_P12/P7/p7.js
@@ -1,10 +1,19 @@
 $(function () {
-    $('button').click(function (e) { 
+
+    $('.noidung').addClass('NDhienra');
+    $('.momo').addClass('dira');
+
+    setInterval(function () {
+        $('.noidung').removeClass('NDhienra');
+        $('.momo').removeClass('dira');
+    }, 7000)
+
+    $('button').click(function (e) {
         $('.noidung').addClass('NDhienra');
         $('.momo').addClass('dira');
     });
 
-    $('.Close, .momo').click(function (e) { 
+    $('.Close, .momo').click(function (e) {
         $('.noidung').removeClass('NDhienra');
         $('.momo').removeClass('dira');
     });
