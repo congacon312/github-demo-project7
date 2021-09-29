10ef93d8db63fad9bb9b6d1a70e1dc08d24e58e5
Nguyễn văn hải <nvhai2408@gmial.com>
2021-09-29 17:02:29 +0700

    upadte P8, thêm css, thêm hiệu ứng, thêm register và login

--- JQuery/P7_P12/P8/p8.js
+++ JQuery/P7_P12/P8/p8.js
@@ -1,6 +1,40 @@
 $(function () {
-    $('button').click(function (e) { 
+
+
+    $('button.displayMenu').click(function (e) { 
         $('ul').toggleClass('hienra');
-        $('button').toggleClass('doimau');
+        $(this).toggleClass('doimau');
+    });
+
+    $('button.dangnhap').click(function (e) { 
+        e.preventDefault();
+        $('.login').addClass('appear');
+    });
+
+    $('button.dangki').click(function (e) { 
+        e.preventDefault();
+        $('.register').addClass('appear');
+    });
+
+    $('.login .nut, .login .close').click(function (e) { 
+        e.preventDefault();
+        $('.login').removeClass('appear');
+    });
+
+    $('.register .nut, .register .close').click(function (e) { 
+        e.preventDefault();
+        $('.register').removeClass('appear');
+    });
+
+    $('.chuyenQuaDangKi').click(function (e) { 
+        e.preventDefault();
+        $('.login').removeClass('appear');
+        $('.register').addClass('appear');
+    });
+
+    $('.ChuyenQuaDangNhap').click(function (e) { 
+        e.preventDefault();
+        $('.register').removeClass('appear');
+        $('.login').addClass('appear');
     });
 });
